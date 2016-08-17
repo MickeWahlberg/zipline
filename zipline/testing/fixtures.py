32d5ce2a0d3d7c338c08e68ddea8febe0117d5e2
Andrew Daniels <adaniels@quantopian.com>
2016-08-17 15:42:06 -0400

    Store start_session and end_session in minute bcolz metadata
    - start_session replaces first_trading_day
    - Add end_session to limit to correct days

--- zipline/testing/fixtures.py
+++ zipline/testing/fixtures.py
@@ -989,9 +989,10 @@ class WithBcolzEquityMinuteBarReader(WithEquityMinuteBarData, WithTmpDir):
         days = cls.equity_minute_bar_days
 
         writer = BcolzMinuteBarWriter(
-            days[0],
             p,
             cls.trading_calendar,
+            days[0],
+            days[-1],
             US_EQUITIES_MINUTES_PER_DAY
         )
         writer.write(cls.make_equity_minute_bar_data())
@@ -1048,9 +1049,10 @@ class WithBcolzFutureMinuteBarReader(WithFutureMinuteBarData, WithTmpDir):
         days = cls.future_minute_bar_days
 
         writer = BcolzMinuteBarWriter(
-            days[0],
             p,
             trading_calendar,
+            days[0],
+            days[-1],
             FUTURES_MINUTES_PER_DAY,
         )
         writer.write(cls.make_future_minute_bar_data())
